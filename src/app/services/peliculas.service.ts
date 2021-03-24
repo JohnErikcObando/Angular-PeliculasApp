4223acd2c57c6eae464c84de7fd9ae6caae7eb0b
JohnErikcObando <Jeobando11@hotmail.com>
2021-03-23 20:38:53 -0500

    implementar servicio de peliculas

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarteleraResponse } from '../interfaces/cartelera-response';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  constructor( private http: HttpClient) { }
+
+  getCartelera():Observable<CarteleraResponse>{
+    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=da13182767a3c771fa0ae8471b0ef919&language=en-US&page=1');
+  }
+
+
+}
